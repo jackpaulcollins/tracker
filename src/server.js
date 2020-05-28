28095ef5dc7184c843b1fcc461c3cfd3e66f2374
Jack Collins <jackpaulcollins@gmail.com>
2020-05-28 13:37:15 -0700

    add mongo db connection

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 8000
+
+if(process.env.NODE_ENV != 'production'){
+  require('dotenv').config()
+}
 
 app.use(cors())
 app.use(express.json())
@@ -10,6 +15,16 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('Hello from Express')
 })
+
+try {
+  mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  console.log('Mongo DB connected')
+} catch(error) {
+  console.log(error)
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`)
